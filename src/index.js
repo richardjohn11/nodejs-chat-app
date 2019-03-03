1685cfca38916d5ccc3ba7ad6bff3937b83b3dfd
Richard John Garcia <richardjohngarcia@yahoo.com>
2019-03-02 17:56:57 -0700

    Add tracking users joining and leaving

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ const {
   generateLocationMessage
 } = require("./utils/messages");
 
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+} = require("./utils/users");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -20,14 +27,21 @@ app.use(express.static(publicDirectoryPath));
 io.on("connection", socket => {
   console.log("New Websocket Connection");
 
-  socket.on("join", ({ username, room }) => {
-    socket.join(room);
+  socket.on("join", (options, callback) => {
+    const { error, user } = addUser({ id: socket.id, ...options });
+
+    if (error) {
+      return callback(error);
+    }
+
+    socket.join(user.room);
 
     socket.emit("message", generateMessage("Welcome!"));
     socket.broadcast
-      .to(room)
-      .emit("message", generateMessage(`${username} has joined!`)); //broadcast to all except the one owned the socket
+      .to(user.room)
+      .emit("message", generateMessage(`${user.username} has joined!`)); //broadcast to all except the one owned the socket
 
+    callback();
     //socket.emit io.emit socket.broadcast.emit
     //io.to.emit socket.broadcast.to.emit
   });
@@ -55,7 +69,14 @@ io.on("connection", socket => {
   });
 
   socket.on("disconnect", () => {
-    io.emit("message", generateMessage("A user has left!"));
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        generateMessage(`${user.username} has left!`)
+      );
+    }
   });
 });
 
